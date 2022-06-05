55825fbb52053aa1e3c362d60b23aa17ea1b032b
MIMBALA <mimbalao@gmail.com>
2022-06-05 11:11:48 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = '0';
+	int a_to_f = 'a';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+
+	while (a_to_f <= 'f')
+	{
+		putchar(a_to_f);
+		a_to_f++;
+	}
+	putchar('\n');
+	return (0);
+}
