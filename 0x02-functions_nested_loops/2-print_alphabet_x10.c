4222f8e9bc67d829372a034803a5b6f53506e06d
MIMBALA <mimbalao@gmail.com>
2022-06-09 08:07:38 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabets ten times
+ * Return: 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+char alpha;
+int counter;
+
+for (counter = 1; counter <= 10; counter++)
+{
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+}
+}
+
