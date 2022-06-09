43ac34bc7f56f590d65d866629c7d589d6231869
MIMBALA <mimbalao@gmail.com>
2022-06-09 07:10:49 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * print putchar followed by a new line
- * Return: 0
+ * main: Print _putchar followed by new line
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
