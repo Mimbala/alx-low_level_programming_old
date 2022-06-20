4dc3e694477e2c1f1e71d80060da92bf51b5d43f
MIMBALA <mimbalao@gmail.com>
2022-06-20 08:38:04 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memset - fill n bytes of memory with a constant value
+ * @s: pointer to memory area
+ * @b: constan value
+ * @n: number of bytes to fill
+ * Return: memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	unsigned int i;
+
+	/*declaring FOR*/
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b; /*add 1 position s*/
+
+		} /*END FOR*/
+
+	return (s);
+}
+
