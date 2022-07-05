b9d4f52329618b159a1bbf9797352f2b1edbfd93
MIMBALA <mimbalao@gmail.com>
2022-07-05 15:17:15 -0700

    task3preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,11 +1,13 @@
-#ifndef _FUNCTION_LIKE_MACRO_H_
-#define _FUNCTION_LIKE_MACRO_H_
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
+
 /**
  * FUNCTION_LIKE_MACRO - function-like macro ABS(x) that computes
  * the absolute value of a number x.
  * @x: Number to compute
  */
-#define ABS(x) (((x) > (0)) ? (x) : (x) * -1)
+
+#define ABS(x) (((x) < 0) ? (-x) : (x))
 
 #endit
 
