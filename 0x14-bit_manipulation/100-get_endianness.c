acef0e59714cf61e8344d4eabe934958819e9c89
MIMBALA <mimbalao@gmail.com>
2022-07-30 00:46:35 -0700

    Betty style correction to task 5 and 100

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -7,9 +7,9 @@
 
 int get_endianness(void)
 {
-	/* have a 4 byte int hold some hex number 0x00000001
-	 test if first byte stored is 00 or 01
-	 big endian would store 00, little would store 01 */
+	/* have a 4 byte int hold some hex number 0x00000001 */
+	/* test if first byte stored is 00 or 01 */
+	/* big endian would store 00, little would store 01 */
 
 	int n = 0x00000001;
 	char *c = (char *)&n;
