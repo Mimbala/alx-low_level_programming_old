dfc5c37165af2329766f4316a8bfd9b70799b6cd
MIMBALA <mimbalao@gmail.com>
2022-08-03 04:51:21 -0700

    fixed task 0 again

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,19 +1,16 @@
-#include <time.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
  * main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+ * Return: 0
+ */
 
 int main(void)
 {
 	int i;
-	
+
 	i = 0;
 	positive_or_negative(i);
-	
+
 	return (0);
 }
